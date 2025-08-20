fe12e42f759b32fd47daebb4c454477df094e722
Samarth Garg <samarthgarg108@gmail.com>
2025-08-20 22:55:51 +0530

    updated

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-if(process.env.NODE_ENV != "production"){
-    require("dotenv").config();
-
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 const express = require("express");
 const app = express();
@@ -15,7 +14,7 @@ const listingsRoutes = require("./routes/listing.js");
 const reviewsRoutes = require("./routes/review.js");
 const userRoutes = require("./routes/user.js");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -23,41 +22,41 @@ const User = require("./models/user.js");
 const { parentPort } = require("worker_threads");
 
 main()
-.then(()=>{
-   console.log("connected to db");
-}).catch((err)=>{
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
     console.log(err);
-})
-async function main(){
-   await mongoose.connect(dbUrl);
+  });
+async function main() {
+  await mongoose.connect(dbUrl);
 }
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
-    touchAfter: 24*3600,
-});
-store.on("error", (err)=>{
-    console.log("ERROR IN MONGODB STORE", err);
-})
-const sessionOptions = {
-    store,
+  mongoUrl: dbUrl,
+  crypto: {
     secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-
-    }
+  },
+  touchAfter: 24 * 3600,
+});
+store.on("error", (err) => {
+  console.log("ERROR IN MONGODB STORE", err);
+});
+const sessionOptions = {
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 
 app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 
@@ -81,22 +80,15 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-
-
-
-
-app.use((req,res,next)=>{
-    res.locals.success=req.flash("success");
-    res.locals.error=req.flash("error");
-    res.locals.currUser=req.user;
-    next();
-})
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
 app.use("/listings", listingsRoutes);
 app.use("/listings/:id/reviews", reviewsRoutes);
-app.use("/",userRoutes);
-
-
+app.use("/", userRoutes);
 
 // app.all('*', (req,res,next)=>{
 //     next(new ExpressError(404, "Page not found"));
@@ -119,15 +111,12 @@ app.use("/",userRoutes);
 //  res.send("successful testing");
 // });
 
-app.get("/", (req, res) => {
-    res.send("Welcome to Wanderslist 🌍");
-  });
-  
-app.use((err, req, res, next)=>{
-    const {statusCode=500, message="Something went wrong"} = err;
-   res.status(statusCode).render("error.ejs", {message});
-})
 
-app.listen(8080, ()=>{
-    console.log("server is listening to port 8080");
-})
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+});
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080");
+});
